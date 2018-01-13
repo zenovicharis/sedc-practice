62701d2bc444b73c22c7d55d413b6f8104f91890
hexadecimalH <zenovicharis@gmail.com>
2018-01-13 11:45:42 +0100

    added animation example

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
 
 module.exports =  {
     entry:'./src/script.js',
     output: {
-        path: 'dist',
+        path: path.resolve(__dirname, "dist"),
         filename: 'app.bundle.js'
     },
     module:{
